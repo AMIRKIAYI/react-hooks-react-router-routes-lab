70412806cf330552aacf7c54e65d1bc760f37f59
AMIRKIAYI <amirkiayi516@gmail.com>
2024-07-21 23:40:22 +0300

    add files

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
 import Movies from "./Movies";
+import Directors from "./Directors";
+import Actors from "./Actors";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movies" exact component={Movies} />
+          <Route path="/directors" exact component={Directors} />
+          <Route path="/actors" exact component={Actors} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
